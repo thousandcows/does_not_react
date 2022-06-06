540b77e83a2482259c261cdce09fc398c31c7f34
thousandcows <milcamboo@gmail.com>
2022-06-06 13:13:35 +0900

    FEAT : 수정 - 기본 html 태그 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function HeaderTag() {
+  return (
+    <header>
+      <h1>
+        <a href="/">Web</a>
+      </h1>
+    </header>
+  );
+}
+
+function NavTag() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+function ArticleTag() {
+  return (
+    <article>
+      {" "}
+      <h2>Welcome</h2>
+      Hello, WEB!
+    </article>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderTag></HeaderTag>
+      <NavTag></NavTag>
+      <ArticleTag></ArticleTag>
     </div>
   );
 }
